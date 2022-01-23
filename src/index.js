83aeefa0b2053a8d40a6ff95cb902ef7b89a8769
Gashun Chagdonov <gowagowa1990@gmail.com>
2022-01-23 13:54:10 +0300

    added loyaut

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hi there..");
+import "./app.css";
+
+const form = document.querySelector("#form");
+const input = form.querySelector('#input')
+
+form.addEventListener('submit', formHandler)
+
+function formHandler(event){
+	event.preventDefault()
+
+	console.log(input.value);
+	input.value = ''
+}
